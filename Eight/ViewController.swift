efb0336bce170e76c49eb720783cd0585dbd71b3
Ivan Demchenko <vanuademchenko@gmail.com>
2021-03-22 16:00:35 +0300

    Add some animation

--- Eight/ViewController.swift
+++ Eight/ViewController.swift
@@ -8,22 +8,50 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
 
     
+    
     @IBOutlet weak var ResultLabel: UILabel!
     
+    @IBOutlet weak var buttounOutlet: UIButton!
+    
     @IBAction func pushButton(_ sender: Any) {
+        ResultLabel.alpha = 0
+        buttounOutlet.alpha = 0
+        
         ResultLabel.text = yesOrNo.randomElement()
+        
+        UIView.animate(withDuration: 0.5, animations: {
+            self.buttounOutlet.alpha = 1
+        } )
+        
+        UIView.animate(withDuration: 1.5, animations: {
+            self.ResultLabel.alpha = 1
+        } )
+        
     }
     
+    
+    
+    
     let yesOrNo = ["Yes", "No"]
+    
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         ResultLabel.text = "Push"
+        ResultLabel.alpha = 0
+        UIView.animate(withDuration: 1.5, animations: {
+            self.ResultLabel.alpha = 1
+        } )
+
+        
     }
 
 
